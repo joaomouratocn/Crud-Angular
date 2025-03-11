f6219cd0f76a52039cd927d88466a5d6ec93ffff
João Mourato <joaomouratocn@gmail.com>
2025-03-11 16:50:18 -0300

    Desenvolvido section main

--- frontend/src/app/components/login-layout/login-layout.component.ts
+++ frontend/src/app/components/login-layout/login-layout.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-login-layout',
@@ -7,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrl: './login-layout.component.css'
 })
 export class LoginLayoutComponent {
-
+  @Input() title:String = "";
+  @Input() primaryButtonText:String = "";
+  @Input() secondaryButtonText:String = "";
 }
